f693c50f3a9ed8b9dea0f7a98aee7dc0cccdd037
Horimoto Yasuhiro <horimoto@clear-code.com>
2022-05-06 12:20:13 +0900

    Update documentation

--- ja/docs/static/documentation_options.js
+++ ja/docs/static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '12.02',
+    VERSION: '12.03',
     LANGUAGE: 'ja',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
