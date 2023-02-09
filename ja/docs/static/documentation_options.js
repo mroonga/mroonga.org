4b884d293f519f50140d9319e80bdf60560da441
Horimoto Yasuhiro <horimoto@clear-code.com>
2023-02-09 12:21:28 +0900

    doc: update document for 13.00

--- ja/docs/static/documentation_options.js
+++ ja/docs/static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'mrn_version',
+    VERSION: '13.00',
     LANGUAGE: 'ja',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
